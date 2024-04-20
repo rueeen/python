c4190ca378d272a0639079f0a8894d256c24cd58
rvalencia <rva.valencia@gmail.com>
2024-04-19 21:28:40 -0400

    commit viernes 19 2

---
+++ ejercicio19.py
@@ -0,0 +1,12 @@
+#Creando listas
+
+nombre_lista = [] #Lista vacia
+
+lista_con_str = ['Perico', 'Maria'] #Lista 2 elementos
+
+lista_con_int = [1, 2, 3, 4, 5, 6 , 7,] #lista con 7 elementos
+print(lista_con_int)
+
+#lista_con_errores = [1, 2 3, 4] error falta coma entre 2 y 3
+
+lista_mixta = ["str", 4, True, 3.5, [] ]
---
+++ ejercicio20.py
@@ -0,0 +1,57 @@
+#CRUD listas
+#CRUD => Create Read Update Delete
+
+#Lista creada fuera para que no se reinicie en cada ciclo
+lst = ['perico', 'maria']
+#indces   0         1
+
+#Crearemos un menu de acciones
+while True:
+    print("1. Agregar")
+    print("2. Leer")
+    print("3. Modificar")
+    print("4. Eliminar")
+    print("0. Salir")
+
+    opcion = input('Ingrese su opcion\n')
+    
+    if opcion == '1':
+        #Agregando elementos a mi lista
+        #La lista contendra nombres de personas
+        nombre = input('Ingrese un nuevo nombre para agregar a lista\n')
+        #para agregar utilizamos metodo .append
+        #primero llamamos a nuestra lista
+        #luego agregamos un .
+        lst.append(nombre)
+        print('Se agrego un nuevo nombre a la lista')
+        
+    elif opcion == '2':
+        print(lst)
+        indice = int(input('Ingrese indice de elemento a mostrar\n'))
+        #Podemos imprimir el dato especifico de la lista con su INDICE
+        #Los indices van desde 0 hata 1 menos el tamaño de la lista
+        print(lst[indice])
+    elif opcion == '3':
+        #Modificando elemento de lista
+        print(lst)
+        #Para modificar necesito 2 cosas
+        #1. el indice del elemento a modificar
+        #2. El nuevo valor
+        indice = int(input("Ingrese indice de elemento a modificar\n"))
+        valor = input("Ingrese nuevo valor\n")
+        #Esto cambia el valor del indice indicado
+        lst[indice] = valor
+        print(lst)
+    elif opcion == '4':
+        #Eliminar elemento de una lista
+        #Se necesita el indice
+        indice = int(input('Ingrese indice de elemento a eliminar\n'))
+        #Eliminamos
+        del lst[indice]
+        #del lst esto elimina toda la lista
+        
+    elif opcion == '0':
+        print('Saliendo de programa')
+        break
+    else:
+        print("Opcion ingresada no valida")
